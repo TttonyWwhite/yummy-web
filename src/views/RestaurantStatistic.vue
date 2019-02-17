435de2c0e077e6950fa8263e86ac1bcabd96738f
illiant <effortblesses@outlook.com>
2019-02-17 21:32:01 +0800

    餐厅统计功能

--- src/views/RestaurantStatistic.vue
+++ src/views/RestaurantStatistic.vue
@@ -2,11 +2,14 @@
 	<div>
 		<el-tabs v-model="activeName" @tab-click="handleClick">
 		    <el-tab-pane label="一周营业额统计" name="first">
-		    	<ve-line :data="businessData"></ve-line>
+		    	<ve-line v-if="activeName == 'first'" :data="businessData"></ve-line>
 		    </el-tab-pane>
-		    <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
-		    <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
-		    <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+		    <el-tab-pane label="新增用户统计" name="second">
+		    	<ve-line v-if="activeName == 'second'" :data="newMemberData"></ve-line>
+		    </el-tab-pane>
+		    <el-tab-pane label="一周订单统计" name="third">
+		    	<ve-line v-if="activeName == 'third'" :data="orderData"></ve-line>
+		    </el-tab-pane>
 		</el-tabs>
 	</div>
 </template>
@@ -18,6 +21,14 @@
 				activeName: 'first',
 				businessData: {
 					columns: ['日期', '营业额'],
+					rows: []
+				},
+				newMemberData: {
+					columns: ['日期', '新增用户'],
+					rows:[]
+				},
+				orderData: {
+					columns: ['日期', '订单数'],
 					rows: []
 				}
 			}
@@ -36,6 +47,24 @@
 				for (var i = 0;i < data.length;i++) {
 					let temp = {'日期': data[i].date, '营业额': data[i].total}
 					this.businessData.rows.push(temp)
+				}
+			})
+
+			this.axios.post('http://localhost:8080/getNewMemberSummary', param).then(response => {
+				//console.log(response)
+				let data = response.data.data
+				for (var i = 0;i < data.length;i++) {
+					let temp = {'日期': data[i].str, '新增用户': data[i].count}
+					this.newMemberData.rows.push(temp)
+				}
+			})
+
+			this.axios.post('http://localhost:8080/getOrderSummary', param).then(response => {
+				let data = response.data.data
+				data.reverse()
+				for (var i = 0;i < data.length;i++) {
+					let temp = {'日期': data[i].str, '订单数': data[i].count}
+					this.orderData.rows.push(temp)
 				}
 			})
 		}	
