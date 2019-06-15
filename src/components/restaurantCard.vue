de03ddebc54f982a81ec907295eefbdc7d4650ad
illiant <effortblesses@outlook.com>
2019-06-15 14:09:15 +0800

    首页添加评分和配送时间数据

--- src/components/restaurantCard.vue
+++ src/components/restaurantCard.vue
@@ -8,23 +8,27 @@
             <el-col>
                 <img :src="imgUrl" width="70" height="70" style="float: left">
                 <div style="float: left; text-align: left; margin-left: 20px">{{shopName}}
-                    <br>评分: {{rate}}
-                    <br>配送费: {{ deliveryCost }} </div>
+                    <br><span><Rate :value="rate" :readonly="true" size='15px'/></span>
+                    <span style="color: #a3a3a3; font-size: 14px; margin-top: -10px">配送费: {{ deliveryCost }}</span> </div>
 
             </el-col>
         </el-row>
 
         <el-row>
             <el-col>
-                <span style="float: left;">{{deliveryTime}} 分</span>
+                <span style="float: left; margin-left: 12px; font-size: 12px; color: #a3a3a3;">{{deliveryTime}} 分钟</span>
             </el-col>
         </el-row>
     </div>
 </template>
 
 <script>
+    import Rate from 'vue-tiny-rate'
     export default {
         name: 'card',
+        components: {
+          Rate
+        },
         props: {
             shopName: {
                 type: String,
